a5e3929611125f0daaed5de457d0d3250af86de9
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-08-22 13:16:13 +0100

    s3 post method
    to read data using post method in S3

--- src/routes/s3route.js
+++ src/routes/s3route.js
@@ -2,7 +2,7 @@ import { getS3Controller } from '../services/controller/readS3data.js'
 
 const getS3 = [
   {
-    method: 'GET',
+    method: 'POST',
     path: '/getS3',
     handler: getS3Controller
   }
