b7eda7b917c134df3f775d000e5f3885836259ce
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-08-22 18:12:20 +0100

    s3 change
    s3 changes for get operation

--- src/routes/s3route.js
+++ src/routes/s3route.js
@@ -2,7 +2,7 @@ import { getS3Controller } from '../services/controller/readS3data.js'
 
 const getS3 = [
   {
-    method: 'POST',
+    method: 'GET',
     path: '/getS3/{requestId}',
     handler: getS3Controller
   }
