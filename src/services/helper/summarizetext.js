90c8d7a8682b726f3bbb0fe79a0883f7a0f2568b
Karthick Muthukrishnan <karthick.muthukrishnan@cognizant.com>
2025-08-26 22:58:15 +0100

    s3 changes
    code change for response

--- src/services/helper/summarizetext.js
+++ src/services/helper/summarizetext.js
@@ -150,7 +150,7 @@ async function processWithBedrockAndWriteToS3(requestId, prompt) {
     const s3Command = new PutObjectCommand({
       Bucket: 'dev-aqie-docanalysis-c63f2',
       Key: `responses/${requestId}.json`,
-      Body: response.content,
+      Body: responseBodynew.content,
       ContentType: 'application/json'
     });
     logger.info('S3 upload ended')
